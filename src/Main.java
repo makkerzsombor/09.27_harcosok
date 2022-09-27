4232a9ae578277adffac7b043bdd04cdb8394a9f
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-27 16:22:14 +0200

    3/b test 1

--- src/Main.java
+++ src/Main.java
@@ -17,10 +17,10 @@ public class Main {
         }
         kiir();
         Scanner sc = new Scanner(System.in);
-        System.out.print("Kérem adja meg, hogy mi legyen a harcosának a neve");
+        System.out.print("Kérem adja meg, hogy mi legyen a harcosának a neve:");
         String harcosNeve = sc.nextLine();
         while(harcosNeve.equals("")){
-            System.out.print("\nKérem adja meg, hogy mi legyen a harcosának a neve");
+            System.out.print("\nKérem adja meg, hogy mi legyen a harcosának a neve:");
             harcosNeve = sc.nextLine();
         }
         System.out.println("\n Kérem adja meg a harcos classját (1-3):");
@@ -30,8 +30,27 @@ public class Main {
             System.out.println("Rossz számot adott meg a szám minimum 1 maximum 3 lehet.");
             System.out.print("\n Kérem adja meg a harcos classját (1-3):");
             harcosClass = sc.nextInt();
+            sc.nextLine();
         }
-        Harcos sHarcos = new Harcos(harcosNeve,harcosClass);
+        Harcos sHarcos = new Harcos(harcosNeve,(harcosClass));
+        harcosok.add(sHarcos);
+
+        //körök 3/b
+        String bekert = "m";
+        while(!bekert.equals("c")){
+            System.out.println("Kérem adja meg, hogy mit szeretne tenni." +
+                    "\n (a) Megküzdeni egy harcossal." +
+                    "\n (b) Gyógyulni." +
+                    "\n (c) kilpéni.");
+            bekert = sc.nextLine();
+            if (bekert.equals("a")){
+                System.out.println("Válassza ki, hogy melyik harcossal szeretne megküzdeni (1-3)");
+                int szambe = sc.nextInt();
+                sc.nextLine();
+                sHarcos.megkuzd(harcosok.get(szambe));
+            }
+        }
+
     }
     public static void fel2b(String fileName) throws IOException {
         harcosok = new ArrayList<>();
@@ -53,7 +72,6 @@ public class Main {
             System.out.println(harcosok.get(i).toString());
         }
     }
-
 
 
 }
