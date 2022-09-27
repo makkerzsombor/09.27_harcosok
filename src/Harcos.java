39c1bec635a70bc7ef1adb59349394c5788f8387
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-27 14:04:06 +0200

    Main java and Harcos java created

---
+++ src/Harcos.java
@@ -0,0 +1,12 @@
+public class Harcos {
+    private String nev;
+    private Integer szint;
+    private Integer tapasztalat;
+    private Integer eletero;
+    private Integer alapEletero;
+    private Integer alapSebzes;
+
+    public Harcos(String nev, Integer statuszSablon) {
+        this.nev = nev;
+    }
+}
